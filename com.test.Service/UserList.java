c9bde73c6975ecebe571b818a0c630c6910f2ff6
rails_project <a1722yz@aiit.ac.jp>
2019-06-19 11:00:35 +0900

    Create UserList.java

---
+++ com.test.Service/UserList.java
@@ -0,0 +1,58 @@
+package com.test.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.Bean.UserBean;
+import com.test.Dao.ReadCsv;
+
+/**
+ * Servlet implementation class StudentList
+ */
+@WebServlet("/UserList")
+public class UserList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext sc = getServletContext();
+		UserBean sbn = new UserBean();
+		ReadCsv rdc = new ReadCsv();
+		List<UserBean> dataList = rdc.importCsv("user.csv");
+
+		request.setAttribute("users",dataList);
+
+	    // 画面の表示
+		RequestDispatcher rd = sc.getRequestDispatcher("/userlist.jsp");
+		rd.forward( request, response );
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
